087edc95120f24c00b07abc24a60ff75acd2da58
TAKANO `takano32' Mitsuhiro <takano32@gmail.com>
2015-08-01 00:07:28 +0900

    zabbixapi gem

--- app.rb
+++ app.rb
@@ -12,9 +12,17 @@ $zabbix_config = Pit.get('zabbix',
   password: 'zabbix password',
 })
 
-
+$zabbix = ZabbixApi.connect(
+  :url => 'http://zabbix.conbu.net/api_jsonrpc.php',
+  :user => $zabbix_config[:user],
+  :password => $zabbix_config[:password]
+)
 
 get '/' do
-  
+  version = $zabbix.query(
+    :method => "apiinfo.version",
+    :params => {}
+  )
+  version
 end
 
