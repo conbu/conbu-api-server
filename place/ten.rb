f3d27e0cd2cc60809571edb64154dc1ffcfe60de
TAKANO `takano32' Mitsuhiro <takano32@gmail.com>
2015-08-02 10:16:04 +0900

    fix place/ten.rb

--- place/ten.rb
+++ place/ten.rb
@@ -2,44 +2,30 @@
 
 @place = {}
 @place[:entrance] = [
-  :ap001,
   :ap101,
 ]
 
 @place[:unice] = [
-  :ap002,
-  :ap003,
-  :ap004,
   :ap102,
   :ap103,
   :ap104,
 ]
 
 @place[:unit] = [
-  :ap005,
-  :ap006,
-  :ap007,
-  :ap008,
-  :ap009,
-  :ap010,
+  :ap001,
   :ap105,
   :ap106,
   :ap107,
   :ap108,
   :ap109,
-  :ap110,
 ]
 
 @place[:saloon] = [
-  :ap011,
-  :ap012,
-  :ap013,
+  :ap110,
   :ap111,
   :ap112,
 ]
 
-@place[:total] = [
-  :ap014, :ap015, :ap016,
-  :ap113, :ap114, :ap115, :ap116,
-] + @place[:entrance] + @place[:unice] + @place[:unit] + @place[:saloon]
+@place[:total] = @place[:entrance] + @place[:unice] + @place[:unit] + @place[:saloon]
 @place[:total] = @place[:total].sort
+
