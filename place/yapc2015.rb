fce0790f65fa5dffdb630ece21aed7046dfaba49
TAKANO `takano32' Mitsuhiro <takano32@gmail.com>
2015-08-19 16:46:34 +0900

    convert all Access Point names to symbol

--- place/yapc2015.rb
+++ place/yapc2015.rb
@@ -24,42 +24,42 @@ __END__
 YAPC:
  6F:
   Reception:
-   AP-005
+   :ap005
   Lobby:
-   AP-006
+   :ap006
   D Track:
   E Track:
-   AP-003
-   AP-004
-   AP-102
+   :ap003
+   :ap004
+   :ap102
  7F:
   Reception:
-   AP-007
-   AP-008
+   :ap007
+   :ap008
   Lobby1:
-   AP-011
-   AP-012
+   :ap011
+   :ap012
   Lobby2:
-   AP-015
-   AP-016
+   :ap015
+   :ap016
   A Track:
-   AP-017
-   AP-018
-   AP-019
-   AP-020
-   AP-105
-   AP-106
-   AP-107
-   AP-108
-   AP-109
-   AP-110
-   AP-111
-   AP-112
+   :ap017
+   :ap018
+   :ap019
+   :ap020
+   :ap105
+   :ap106
+   :ap107
+   :ap108
+   :ap109
+   :ap110
+   :ap111
+   :ap112
   B Track:
-   AP-009
-   AP-010
-   AP-103
+   :ap009
+   :ap010
+   :ap103
   C Track:
-   AP-014
-   AP-104
+   :ap014
+   :ap104
 
