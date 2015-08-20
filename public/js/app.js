8711febe4efbdc0082ce456ac5237be6f5ee0f54
ikosin <jun62626@gmail.com>
2015-08-21 08:07:54 +0900

    get associations by ap and add places (7F)

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,10 @@
   var heatmapInstance_c = h337.create(config);
   var heatmapInstance_d = h337.create(config);
   var heatmapInstance_e = h337.create(config);
-  var heatmapInstance_reception = h337.create(config);
+  var heatmapInstance_reception_6f = h337.create(config);
+  var heatmapInstance_reception_7f = h337.create(config);
+  var heatmapInstance_lobby_1 = h337.create(config);
+  var heatmapInstance_lobby_2 = h337.create(config);
 
   var _dataPoint = {
     radius: 150,
@@ -23,69 +26,121 @@
   };
 
   var track_a = [{
-    place: "track_a",
-    points: [{x: 700, y: 1580}, {x: 900, y: 1530}, {x: 800, y: 1530}, {x: 870, y: 1470}, {x: 730, y: 1470}, {x: 880, y: 1650}, {x: 850, y: 1650}, {x: 750, y: 1660}],
-    value: 1000,
-    max: 1000 * 1.0
-  }]
+      place: "ap017",
+      points: [{x: 700, y: 1320}],
+      max: 1000 * 1.0 / 12
+    },{
+      place: "ap018",
+      points: [{x: 850, y: 1320}],
+    },{
+      place: "ap105",
+      points: [{x: 570, y: 1440}],
+    },{
+      place: "ap107",
+      points: [{x: 750, y: 1440}],
+    },{
+      place: "ap111",
+      points: [{x: 950, y: 1430}],
+    },{
+      place: "ap108",
+      points: [{x: 700, y: 1520}],
+    },{
+      place: "ap110",
+      points: [{x: 880, y: 1520}],
+    },{
+      place: "ap106",
+      points: [{x: 570, y: 1600}],
+    },{
+      place: "ap109",
+      points: [{x: 750, y: 1600}],
+    },{
+      place: "ap112",
+      points: [{x: 950, y: 1600}],
+    },{
+      place: "ap019",
+      points: [{x: 700, y: 1700}],
+    },{
+      place: "ap020",
+      points: [{x: 850, y: 1700}]
+    }];
   var track_b = [{
-    place: "track_b",
-    points: [{x: 1100, y: 1120}, {x: 1200, y: 1120}, {x: 1150, y: 1150}],
-    value: 100,
-    max: 100 * 1.0
-  }]
+      place: "ap010",
+      points: [{x: 1090, y: 1130}],
+      max: 100 * 1.0 / 3
+    },{
+      place: "ap103",
+      points: [{x: 1150, y: 1090}],
+    },{
+      place: "ap009",
+      points: [{x: 1210, y: 1130}]
+    }];
   var track_c = [{
-    place: "track_c",
-    points: [{x: 1100, y: 2030}, {x: 1200, y: 1980}, {x: 1150, y: 2000}],
-    value: 122,
-    max: 122 * 1.0
-  }]
+      place: "ap014",
+      points: [{x: 1090, y: 2000}],
+      max: 122 * 1.0 / 2
+    },{
+      place: "ap104",
+      points: [{x: 1250, y: 1960}]
+    }];
   var track_d = [{
       place: "ap001",
       points: [{x: 630, y: 350}],
-      value: 288,
       max: 288 * 0.6 / 3
     },{
       place: "ap101",
       points: [{x: 760, y: 440}],
-      value: 288,
-      max: 288 * 0.6 / 3
     },{
       place: "ap002",
-      points: [{x: 869, y: 350}],
-      value: 288,
-      max: 288 * 0.6 / 3
+      points: [{x: 869, y: 350}]
     }];
   var track_e = [{
       place: "ap003",
       points: [{x: 630, y: 530}],
-      value: 288,
       max: 288 * 0.6 / 3
     },{
       place: "ap102",
       points: [{x: 770, y: 610}],
-      value: 288,
-      max: 288 * 0.6 / 3
     },{
       place: "ap004",
-      points: [{x: 890, y: 520}],
-      value: 288,
-      max: 288 * 0.6 / 3
-    }]
-  var reception = [{
+      points: [{x: 890, y: 520}]
+    }];
+  var reception_6f = [{
       place: "ap005",
       points: [{x: 990, y: 390}],
-      max: 50
+      max: 40
     },{
       place: "ap006",
-      points: [{x: 990, y: 550}],
-      max: 50
+      points: [{x: 990, y: 550}]
+    }];
+  var reception_7f = [{
+      place: "ap007",
+      points: [{x: 1220, y: 1430}],
+      max: 40
+    },{
+      place: "ap008",
+      points: [{x: 1220, y: 1620}],
+    }];
+  var lobby_1 = [{
+      place: "ap012",
+      points: [{x: 610, y: 1170}],
+      max: 40
+    },{
+      place: "ap011",
+      points: [{x: 920, y: 1170}],
+    }];
+  var lobby_2 = [{
+      place: "ap016",
+      points: [{x: 610, y: 1890}],
+      max: 40
+    },{
+      place: "ap015",
+      points: [{x: 920, y: 1890}],
     }];
 
   function getData(track) {
     var dataPoints = [];
     track.forEach(function(ap) {
-      var associations = getAssociations(ap.place)
+      var associations = getAssociations(ap.place);
       ap.points.forEach(function(point) {
         var clonedDataPoint = (JSON.parse(JSON.stringify(_dataPoint)));
         clonedDataPoint.x = point.x;
@@ -120,19 +175,22 @@
     return associations;
   }
 
-  function lender() {
+  function start() {
 
     var dataPoints = [];
-    heatmapInstance_a.setData({data:[]}).setData(getData(track_a));
-    heatmapInstance_b.setData({data:[]}).setData(getData(track_b));
-    heatmapInstance_c.setData({data:[]}).setData(getData(track_c));
-    heatmapInstance_d.setData({data:[]}).setData(getData(track_d));
-    heatmapInstance_e.setData({data:[]}).setData(getData(track_e));
-    heatmapInstance_reception.setData({data:[]}).setData(getData(reception));
-    setTimeout(lender, 10000);
+    heatmapInstance_a.setData(getData(track_a));
+    heatmapInstance_b.setData(getData(track_b));
+    heatmapInstance_c.setData(getData(track_c));
+    heatmapInstance_d.setData(getData(track_d));
+    heatmapInstance_e.setData(getData(track_e));
+    heatmapInstance_reception_6f.setData(getData(reception_6f));
+    heatmapInstance_reception_7f.setData(getData(reception_7f));
+    heatmapInstance_lobby_1.setData(getData(lobby_1));
+    heatmapInstance_lobby_2.setData(getData(lobby_2));
+    setTimeout(start, 10000);
   }
 
   window.onload = function() {
-    lender();
+    start();
   };
 })();
