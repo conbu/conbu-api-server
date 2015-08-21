190ed250a932b9e3416740764caccb0ce6890ac4
ikosin <jun62626@gmail.com>
2015-08-21 15:27:41 +0900

    rename ap, improve performance, adjust max capacity

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,7 @@
   var config = {
     container: document.getElementById('top-view')
   };
-  var heatmapInstance_a = h337.create(config);
-  var heatmapInstance_b = h337.create(config);
-  var heatmapInstance_c = h337.create(config);
-  var heatmapInstance_d = h337.create(config);
-  var heatmapInstance_e = h337.create(config);
-  var heatmapInstance_reception_6f = h337.create(config);
-  var heatmapInstance_reception_7f = h337.create(config);
-  var heatmapInstance_lobby_1 = h337.create(config);
-  var heatmapInstance_lobby_2 = h337.create(config);
-
+  var heatmapInstance = h337.create(config);
   var _dataPoint = {
     radius: 150,
 //    opacity: 1,
@@ -26,121 +17,124 @@
   };
 
   var track_a = [{
-      place: "ap017",
+      place: "AP-017",
       points: [{x: 700, y: 1320}],
-      max: 1000 * 1.0 / 12
+      max: 1000 * 2.0 / 12
     },{
-      place: "ap018",
+      place: "AP-018",
       points: [{x: 850, y: 1320}],
     },{
-      place: "ap105",
+      place: "AP-105",
       points: [{x: 570, y: 1440}],
     },{
-      place: "ap107",
+      place: "AP-107",
       points: [{x: 750, y: 1440}],
     },{
-      place: "ap111",
+      place: "AP-111",
       points: [{x: 950, y: 1430}],
     },{
-      place: "ap108",
+      place: "AP-108",
       points: [{x: 700, y: 1520}],
     },{
-      place: "ap110",
+      place: "AP-110",
       points: [{x: 880, y: 1520}],
     },{
-      place: "ap106",
+      place: "AP-106",
       points: [{x: 570, y: 1600}],
     },{
-      place: "ap109",
+      place: "AP-109",
       points: [{x: 750, y: 1600}],
     },{
-      place: "ap112",
+      place: "AP-112",
       points: [{x: 950, y: 1600}],
     },{
-      place: "ap019",
+      place: "AP-019",
       points: [{x: 700, y: 1700}],
     },{
-      place: "ap020",
+      place: "AP-020",
       points: [{x: 850, y: 1700}]
     }];
   var track_b = [{
-      place: "ap010",
+      place: "AP-010",
       points: [{x: 1090, y: 1130}],
-      max: 100 * 1.0 / 3
+      max: 100 * 2.0 / 3
     },{
-      place: "ap103",
+      place: "AP-103",
       points: [{x: 1150, y: 1090}],
     },{
-      place: "ap009",
+      place: "AP-009",
       points: [{x: 1210, y: 1130}]
     }];
   var track_c = [{
-      place: "ap014",
-      points: [{x: 1090, y: 2000}],
-      max: 122 * 1.0 / 2
+      place: "AP-014",
+      points: [{x: 1150, y: 2000}],
+      max: 122 * 2.0 / 3
     },{
-      place: "ap104",
+      place: "AP-104",
       points: [{x: 1250, y: 1960}]
+    },{
+      place: "AP-134",
+      points: [{x: 1050, y: 1930}]
     }];
   var track_d = [{
-      place: "ap001",
+      place: "AP-001",
       points: [{x: 630, y: 350}],
-      max: 288 * 1.0 / 3
+      max: 288 * 2.0 / 3
     },{
-      place: "ap101",
+      place: "AP-101",
       points: [{x: 760, y: 440}],
     },{
-      place: "ap002",
+      place: "AP-002",
       points: [{x: 869, y: 350}]
     }];
   var track_e = [{
-      place: "ap003",
+      place: "AP-003",
       points: [{x: 630, y: 530}],
-      max: 288 * 1.0 / 3
+      max: 288 * 2.0 / 3
     },{
-      place: "ap102",
+      place: "AP-102",
       points: [{x: 770, y: 610}],
     },{
-      place: "ap004",
+      place: "AP-004",
       points: [{x: 890, y: 520}]
     }];
   var reception_6f = [{
-      place: "ap005",
+      place: "AP-005",
       points: [{x: 990, y: 390}],
-      max: 40
+      max: 80 / 2
     },{
-      place: "ap006",
+      place: "AP-006",
       points: [{x: 990, y: 550}]
     }];
   var reception_7f = [{
-      place: "ap007",
+      place: "AP-007",
       points: [{x: 1220, y: 1430}],
-      max: 40
+      max: 80 / 2
     },{
-      place: "ap008",
+      place: "AP-008",
       points: [{x: 1220, y: 1620}],
     }];
   var lobby_1 = [{
-      place: "ap012",
+      place: "AP-012",
       points: [{x: 610, y: 1170}],
-      max: 40
+      max: 80 / 2
     },{
-      place: "ap011",
+      place: "AP-011",
       points: [{x: 920, y: 1170}],
     }];
   var lobby_2 = [{
-      place: "ap016",
+      place: "AP-016",
       points: [{x: 610, y: 1890}],
-      max: 40
+      max: 80 / 2
     },{
-      place: "ap015",
+      place: "AP-015",
       points: [{x: 920, y: 1890}],
     }];
 
-  function getData(track) {
+  function getDataPoints(track) {
     var dataPoints = [];
     track.forEach(function(ap) {
-      var associations = getAssociations(ap.place);
+      var associations = getAssociations(ap.place) * (1000 / track[0].max);
       ap.points.forEach(function(point) {
         var clonedDataPoint = (JSON.parse(JSON.stringify(_dataPoint)));
         clonedDataPoint.x = point.x;
@@ -149,12 +143,7 @@
         dataPoints.push(clonedDataPoint);
       });
     });
-    var data = {
-      max: track[0].max,
-      min: 0,
-      data: dataPoints
-    };
-    return data;
+    return dataPoints;
   }
 
   function getAssociations(place) {
@@ -189,15 +178,21 @@
   function start() {
 
     var dataPoints = [];
-    heatmapInstance_a.setData(getData(track_a));
-    heatmapInstance_b.setData(getData(track_b));
-    heatmapInstance_c.setData(getData(track_c));
-    heatmapInstance_d.setData(getData(track_d));
-    heatmapInstance_e.setData(getData(track_e));
-    heatmapInstance_reception_6f.setData(getData(reception_6f));
-    heatmapInstance_reception_7f.setData(getData(reception_7f));
-    heatmapInstance_lobby_1.setData(getData(lobby_1));
-    heatmapInstance_lobby_2.setData(getData(lobby_2));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(track_a));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(track_b));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(track_c));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(track_d));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(track_e));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(reception_6f));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(reception_7f));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(lobby_1));
+    dataPoints = dataPoints.concat(dataPoints, getDataPoints(lobby_2));
+    var data = {
+      max: 1000,
+      min: 0,
+      data: dataPoints
+    };
+    heatmapInstance.setData(data);
 
     updateTime();
 
